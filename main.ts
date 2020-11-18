e07f3ed5c5a6247de8f6946b4ca3f778eebd192b
410983045 <71751207+410983045@users.noreply.github.com>
2020-11-18 15:26:10 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
